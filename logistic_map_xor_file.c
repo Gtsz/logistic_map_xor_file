8727810155b308714a9092a22b755febd90a82d2
AlephO <gtsz@aleph.gq>
2018-06-30 05:14:41 +0800

    simpified

--- logistic_map_xor_file.c
+++ logistic_map_xor_file.c
@@ -1,21 +1,24 @@
 #include <Windows.h>
 #include <stdio.h>
 
+#define BUFFER_LENGTH 4096
+#define MAX_ARRAY_LENGTH 2048
+#define SKIP_N 30 // It's a key value
+#define EXT_NAME ".lmx"
 int main(int argc, char *argv[]) {
-	const int BUFFER_LENGTH = 4096;
 	unsigned char buffer[BUFFER_LENGTH];
 	FILE *file_in, *file_out;
-	char arr[2048], *ptr, *ext_name = ".lmx";
+	char arr[MAX_ARRAY_LENGTH], *ptr;
 	int i, j;
 	double x = 0.2, y;
 	
 	if ((file_in = fopen(argv[1], "rb")) != NULL) {
-		printf ("Type your arr:\n");
+		printf ("Type your key here:\n");
 		scanf ("%s", arr);
 		
 		for (i = 0, ptr = arr; *ptr != '\0'; ptr++, i++) {
 			y = 1/(double)*ptr;
-			for (j = i+30; j >= 0; j--)
+			for (j = i+SKIP_N; j >= 0; j--)
 				y *= 4 - 4*y;
 			x = (x+y) / 2;
 		}
@@ -23,8 +26,8 @@ int main(int argc, char *argv[]) {
 		
 		for (i = 0, ptr = argv[1]; *ptr != '\0'; ptr++, i++)
 			arr[i] = *ptr;
-		for ( ; *ext_name != '\0'; ext_name++)
-			arr[i++] = *ext_name;
+		for (ptr = EXT_NAME; *ptr != '\0'; ptr++)
+			arr[i++] = *ptr;
 		arr[i] = '\0';
 		printf("Output: %s\n", arr);
 		file_out = fopen(arr, "wb");
